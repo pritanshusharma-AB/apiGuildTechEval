e35f600aaca5dbb085e6a7edca84ee1447d6eadf
usualiron6413 <56013989+usualiron6413@users.noreply.github.com>
2025-05-07 14:50:07 -0400

    feat: add graphql code-first

--- consumer-loyalty-backend/src/app.module.ts
+++ consumer-loyalty-backend/src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HelloResolver } from './hello.resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true, // Enable code-first schema generation
+      playground: false, // Enable playground for development
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HelloResolver],
 })
 export class AppModule {}
