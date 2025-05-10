39fce9a8f713b0419e4f618840e2a57f3d1394c0
usualiron6413 <56013989+usualiron6413@users.noreply.github.com>
2025-05-10 13:16:13 -0400

    fix: bootstap call

--- consumer-loyalty-backend/src/main.ts
+++ consumer-loyalty-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    console.log('Application started on port', PORT);
+  })
+  .catch((err) => {
+    console.error('Error starting the application:', err);
+    process.exit(1);
+  });
